f5cf66eb4d30932b38997d7d10c53c76e16aebf9
Quang PHAM <q.pham@prodaxis.fr>
2019-04-05 18:04:54 +0200

    Initial commit

---
+++ DatasetLoader.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+filepath_dict = {'yelp':   'data/yelp_labelled.txt',
+                 'amazon': 'data/amazon_cells_labelled.txt',
+                 'imdb':   'data/imdb_labelled.txt'}
+
+df_list = []
+
+for source, filepath in filepath_dict.items():
+    df = pd.read_csv(filepath, names=['sentence', 'label'], sep='\t')
+    df['source'] = source  # Add another column filled with the source name
+    df_list.append(df)
+
+df = pd.concat(df_list)
+print(df.iloc[0])
